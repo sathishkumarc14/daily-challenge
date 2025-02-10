872e1510b56140483e15a7b32e2749c36ee903b1
sathishkumarc14 <45957721+sathishkumarc14@users.noreply.github.com>
2025-02-10 01:43:37 -0600

    refactor

---
+++ day001-two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const numToIndex = new Map();
+    for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+            const complement = target - num;
+            
+            if (numToIndex.has(complement)) {
+                return [numToIndex.get(complement), i];
+            }
+            
+            numToIndex.set(num, i);
+        } 
+};
